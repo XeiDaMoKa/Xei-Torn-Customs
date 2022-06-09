a3e50a7d37cf9c7198154cfb2d8e00cb306c3b9b
XeiDaMoKa <Xeidamoka@hotmail.com>
2022-06-09 19:16:18 +0100

    creating events files
    files to run only on torn.com/events*

---
+++ ● Xei • Torn  • Events.js
@@ -0,0 +1,18 @@
+// Color of Train Events
+
+$('.new .container-body:contains(trained)')
+
+    .attr
+
+        ('style', 'background-image:linear-gradient(to right , rgb(25,0,38,1)  , rgb(25,0,38,1)  , rgb(25,0,38,1)  , rgb(25,0,38,1) , rgb(25,0,38,1) , rgb(25,0,38,1) , rgb(0,0,100,1) , rgb(0,0,100,1))');;
+
+
+
+
+// Color of Train Events
+
+$(' .container-body:contains(sent)')
+
+    .attr
+
+        ('style', 'background-image:linear-gradient(to right , rgb(50,50,50,1)  , rgb(50,50,50,1)  , rgb(50,50,50,1)  , rgb(50,50,50,1) , rgb(50,50,50,1) , rgb(50,50,50,1) , rgb(0,0,100,1) , rgb(0,0,100,1))');;
