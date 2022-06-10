7ebb9751c4a84bb8ed94cee3725a571c7459dd5c
XeiDaMoKa <Xeidamoka@hotmail.com>
2022-06-10 05:58:01 +0100

    beautifulying

--- ● Xei • Torn • Events •/● Xei • Torn • Events.js
+++ ● Xei • Torn • Events •/● Xei • Torn • Events.js
@@ -1,18 +1,18 @@
 // Color of Train Events
 
-$('.new .container-body:contains(trained)')
+    $('.new .container-body:contains(trained)')
 
-    .attr
+        .attr
 
-        ('style', 'background-image:linear-gradient(to right , rgb(25,0,38,1)  , rgb(25,0,38,1)  , rgb(25,0,38,1)  , rgb(25,0,38,1) , rgb(25,0,38,1) , rgb(25,0,38,1) , rgb(0,0,100,1) , rgb(0,0,100,1))');;
+            ('style', 'background-image:linear-gradient(to right , rgb(25,0,38,1)  , rgb(25,0,38,1)  , rgb(25,0,38,1)  , rgb(25,0,38,1) , rgb(25,0,38,1) , rgb(25,0,38,1) , rgb(0,0,100,1) , rgb(0,0,100,1))');;
 
 
 
 
 // Color of Train Events
 
-$(' .container-body:contains(sent)')
+    $(' .container-body:contains(sent)')
 
-    .attr
+        .attr
 
-        ('style', 'background-image:linear-gradient(to right , rgb(50,50,50,1)  , rgb(50,50,50,1)  , rgb(50,50,50,1)  , rgb(50,50,50,1) , rgb(50,50,50,1) , rgb(50,50,50,1) , rgb(0,0,100,1) , rgb(0,0,100,1))');;
+            ('style', 'background-image:linear-gradient(to right , rgb(50,50,50,1)  , rgb(50,50,50,1)  , rgb(50,50,50,1)  , rgb(50,50,50,1) , rgb(50,50,50,1) , rgb(50,50,50,1) , rgb(0,0,100,1) , rgb(0,0,100,1))');;
