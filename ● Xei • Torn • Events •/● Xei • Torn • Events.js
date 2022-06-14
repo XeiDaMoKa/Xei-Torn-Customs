27ceaf33c9793924cd7baabc92e45cce262cdc91
XeiDaMoKa <Xeidamoka@hotmail.com>
2022-06-14 16:06:45 +0100

    update

--- ● Xei • Torn • Events •/● Xei • Torn • Events.js
+++ ● Xei • Torn • Events •/● Xei • Torn • Events.js
@@ -1,18 +1,93 @@
-// Color of Train Events
-
-    $('.new .container-body:contains(trained)')
-
-        .attr
-
-            ('style', 'background-image:linear-gradient(to right , rgb(25,0,38,1)  , rgb(25,0,38,1)  , rgb(25,0,38,1)  , rgb(25,0,38,1) , rgb(25,0,38,1) , rgb(25,0,38,1) , rgb(0,0,100,1) , rgb(0,0,100,1))');;
 
 
 
+// Color of New Race Events
 
-// Color of Train Events
-
-    $(' .container-body:contains(sent)')
+    $(' .new .container-body:contains(finished)')
 
         .attr
 
-            ('style', 'background-image:linear-gradient(to right , rgb(50,50,50,1)  , rgb(50,50,50,1)  , rgb(50,50,50,1)  , rgb(50,50,50,1) , rgb(50,50,50,1) , rgb(50,50,50,1) , rgb(0,0,100,1) , rgb(0,0,100,1))');;
+            ('style', 'background-image:linear-gradient(to right , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #004d00 , #004d00)')
+;
+
+
+
+// Color of Race Events
+
+    $(' .container-body:contains(finished)')
+
+        .attr
+
+            ('style', 'background-image:linear-gradient(to right , #333333 , #333333 , #333333 , #333333 , #333333 , #333333 , #004d00 , #004d00)')
+;
+
+
+
+
+
+// Color of New Bet Events
+
+    $(' .new .container-body:contains(bet)')
+
+        .attr
+
+            ('style', 'background-image:linear-gradient(to right , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #004d66 , #004d66)')
+;
+
+
+
+// Color of Bet Events
+
+    $(' .container-body:contains(bet)')
+
+        .attr
+
+            ('style', 'background-image:linear-gradient(to right , #333333 , #333333 , #333333 , #333333 , #333333 , #333333 , #004d66 , #004d66)')
+;
+
+
+
+
+
+// Color of New Trains by Company Events
+
+    $(' .new .container-body:contains(trained)')
+
+        .attr
+
+            ('style', 'background-image:linear-gradient(to right , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #666600 , #666600)')
+;
+
+
+
+// Color of Trains by Company Events
+
+    $(' .container-body:contains(trained)')
+
+        .attr
+
+            ('style', 'background-image:linear-gradient(to right , #333333 , #333333 , #333333 , #333333 , #333333 , #333333 , #666600 , #666600)')
+;
+
+
+
+
+
+// Color of New Atack Events
+
+    $(' .new .container-body:contains(atacked)')
+
+        .attr
+
+            ('style', 'background-image:linear-gradient(to right , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #1c0028 , #006600 , #006600)')
+;
+
+
+
+// Color of Atack Events
+
+    $(' .container-body:contains(atacked)')
+
+        .attr
+
+            ('style', 'background-image:linear-gradient(to right , #333333 , #333333 , #333333 , #333333 , #333333 , #333333 , #006600 , #006600)')
