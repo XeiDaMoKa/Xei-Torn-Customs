305192cd35468610b0bb795c2cfc42adc8f41b48
XeiDaMoKa <Xeidamoka@hotmail.com>
2022-06-17 06:52:11 +0100

    update iframes

--- iFrames/● Xei • Torn • iFrames.js
+++ iFrames/● Xei • Torn • iFrames.js
@@ -1,22 +1,28 @@
 
+
 // Vault iFrame
 
-    $(' body ')
+    $('body').prepend(
 
-        .prepend(
-
-            "<iframe id='ivault' class='iframes' scrolling='no' style='display:none ; position:fixed ; width:850px ; height:326px ; left:34% ; top:13% ; z-index:99 ; border:10px solid #1a0029 ; outline:1px solid #f50' src= 'https://www.torn.com/properties.php#/p=options&tab=vault' </iframe>"
-        )
+        "<iframe id='ivault' class='iframes' scrolling='no'"
+        +
+        "style='display:none; position:fixed; width:850px; height:326px; left:34%; top:13%;z-index:99; border:10px solid #1a0029 ; outline:1px solid #f50'"
+        +
+        "src= 'https://www.torn.com/properties.php#/p=options&tab=vault' </iframe>"
+    )
+;
+    checkIframeLoaded()
 ;
 
 
-// MouseHover Money Value
 
-    $(' #user-money ').mouseenter(function(){
+// MouseHover Money Value 1 second (needs fix to cancel if mouseleave within the 1 second)
+
+    $('#user-money').mouseenter(function(){
 
         setTimeout(function() {
 
-            $(' #ivault ').show()
+            $('#ivault').show();
 
         }, 1000)
     })
@@ -26,18 +32,29 @@
 
 // Click OutSide to Hide iFrame
 
-    $(' body ').click(function() {
+    $('body').click(function() {
 
-        $('.iframes').hide()
+        $('#ivault').hide()
     })
 ;
 
 
 
-// doesnt work
+// Hide if its Loaded
 
-    $('#ivault').contents()
+    function checkIframeLoaded() {
 
-        .find('#header-root')
+        let a = $("#ivault").contents().find("#header-root , #sidebarroot , #chatRoot , .property-info-cont ");
 
-            .hide();
+            if ($(a).length)
+
+                $(a).hide();
+
+            if (!$(a).length) {
+
+                window.setTimeout(checkIframeLoaded, 250);
+            }
+    }
+;
+
+
