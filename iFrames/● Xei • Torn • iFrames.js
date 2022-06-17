7cf7d3ab7f89fb8fe0d00b38789e5688636d0af4
Miguel Lemos <44626898+XeiDaMoKa@users.noreply.github.com>
2022-06-17 06:55:03 +0100

    Update ● Xei • Torn • iFrames.js

--- iFrames/● Xei • Torn • iFrames.js
+++ iFrames/● Xei • Torn • iFrames.js
@@ -3,7 +3,6 @@
 // Vault iFrame
 
     $('body').prepend(
-
         "<iframe id='ivault' class='iframes' scrolling='no'"
         +
         "style='display:none; position:fixed; width:850px; height:326px; left:34%; top:13%;z-index:99; border:10px solid #1a0029 ; outline:1px solid #f50'"
@@ -19,11 +18,8 @@
 // MouseHover Money Value 1 second (needs fix to cancel if mouseleave within the 1 second)
 
     $('#user-money').mouseenter(function(){
-
         setTimeout(function() {
-
             $('#ivault').show();
-
         }, 1000)
     })
 ;
@@ -33,7 +29,6 @@
 // Click OutSide to Hide iFrame
 
     $('body').click(function() {
-
         $('#ivault').hide()
     })
 ;
@@ -43,15 +38,10 @@
 // Hide if its Loaded
 
     function checkIframeLoaded() {
-
         let a = $("#ivault").contents().find("#header-root , #sidebarroot , #chatRoot , .property-info-cont ");
-
             if ($(a).length)
-
                 $(a).hide();
-
             if (!$(a).length) {
-
                 window.setTimeout(checkIframeLoaded, 250);
             }
     }
